a4c06869492611561e15ba79aa0fe95f64c978de
Adrian Mouat <adrian.mouat@gmail.com>
2016-12-24 20:52:45 +0000

    Use tini and move toward multi-arch support.

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+# TODO: check uname -m and use to build arm/x86_64 image
+
 docker build -t reggie-build --file Dockerfile.build .
 docker run -d --name reggie-temp reggie-build sleep 1h
 docker cp reggie-temp:/go/reggie ./
