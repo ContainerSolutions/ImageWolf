f0f2abf6b0e7964f427e4e10242d8e023c2da1de
Adrian Mouat <adrian.mouat@gmail.com>
2017-01-12 11:55:23 +0000

    Add arch to built filename.

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ docker build -t reggie-build --file Dockerfile.build .
 docker run -d --name reggie-temp reggie-build sleep 1h
 docker cp reggie-temp:/go/reggie ./reggie-$arch
 docker rm -f reggie-temp
-docker build --build-arg ARCH=$arch -t reggie --file Dockerfile.run .
+docker build --build-arg ARCH=$arch -t amouat/reggie-$arch --file Dockerfile.run .
