5500a195428972262c2bb3dbcc22b0cd8cf2bd4f
Adrian Mouat <adrian.mouat@gmail.com>
2017-01-13 12:36:57 +0000

    typo.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ set -e
 #attach to network only works swarm 1.13 and on
 docker pull amouat/registry-reggie
 docker run -d --name registry-reggie --network reggie -p 5000:5000 \
-           -e REGISTRY_NOTIFICATIONS_ENDPOINT_URL=${HOSTNAME}:8000/registryNotifications \
+           -e REGISTRY_NOTIFICATIONS_ENDPOINTS_URL=${HOSTNAME}:8000/registryNotifications \
            amouat/registry-reggie
 
 exec /reggie $@
