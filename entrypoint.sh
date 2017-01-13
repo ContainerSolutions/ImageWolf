e0b9bef5408ced66d18ab7de11b8c3b96ef20104
Adrian Mouat <adrian.mouat@gmail.com>
2017-01-13 14:34:22 +0000

    Don't fail on stop

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,8 @@ trap 'sigterm' TERM
 #attach to network only works swarm 1.13 and on
 docker pull amouat/registry-reggie
 #kill any old registry
-docker stop registry-reggie
-docker rm registry-reggie
+docker stop registry-reggie || true
+docker rm registry-reggie || true
 docker run -d --name registry-reggie --network reggie -p 5000:5000 \
            -e REGISTRY_NOTIFICATIONS_ENDPOINTS_URL=${HOSTNAME}:8000/registryNotifications \
            amouat/registry-reggie
