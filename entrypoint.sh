4877d4e3b64f3ec9958008dc7fa0f4f6498f3fd3
Adrian Mouat <adrian.mouat@gmail.com>
2017-01-13 12:25:10 +0000

    Rogue space!

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 #remember volumes won't work, which makes things trickier
 #could use network pattern I can't remember the name of
 #attach to network only works swarm 1.13 and on
-docker run -d --name registry-reggie --network reggie -p 5000:5000 \ 
+docker run -d --name registry-reggie --network reggie -p 5000:5000 \
            -e REGISTRY_NOTIFICATIONS_ENDPOINT_URL=${HOSTNAME}:8000/registryNotifications \
            amouat/registry-reggie
 
