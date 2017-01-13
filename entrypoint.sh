0df25d79eaeef83f2e50c0c0db818312a1adbcc2
Adrian Mouat <adrian.mouat@gmail.com>
2017-01-13 11:49:06 +0000

    Split out registry in terrible hack.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
+set -euo pipefail
+IFS=$'\n\t'
 set -e
 
-/registry serve /etc/docker/registry/config.yml &
+#remember volumes won't work, which makes things trickier
+#could use network pattern I can't remember the name of
+#attach to network only works swarm 1.13 and on
+docker run -d --name registry-reggie --network reggie -p 5000:5000 \ 
+           -e REGISTRY_NOTIFICATIONS_ENDPOINT_URL=${HOSTNAME}:8000/registryNotifications \
+           registry-reggie
+
 exec /reggie $@
 
