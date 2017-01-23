170ddf06eaa11cf7aaa642c9a53fcc3ee947b25d
Adrian Mouat <adrian.mouat@gmail.com>
2017-01-23 16:21:26 +0000

    Fix compare.

--- compare.sh
+++ compare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Want to compare speed with and without reggie
 
@@ -10,16 +11,23 @@ docker tag amouat/large-image-arm:100 localhost:5000/large-image-arm:100
 docker tag amouat/large-image-arm:100 localhost:6000/large-image-arm:100
 
 start_reggie=$(date +%s%N)
-docker service create --mode global localhost:5000/large_image-arm:100 compare_test
+docker service create --name compare_test_1 --mode global localhost:5000/large_image-arm:100
 docker push localhost:5000/large-image-arm:100
 
-# Want unique code to avoid picking up old versions
-# Could use squash and different file to stop caching
-docker service ls -f name=reggie | awk 'NR==2 {split($4,a,"/"); if (a[1] != a[2]){ exit 1} }'
+ready=$(docker service ls -f name=compare_test_1 | \
+  awk 'NR==2 {split($4,a,"/"); if (a[1] != a[2]){ echo 1; exit 1} }')
+
+while [[ $ready != "0" ]]; do
+  ready=$(docker service ls -f name=compare_test_1 | \
+    awk 'NR==2 {split($4,a,"/"); if (a[1] != a[2]){ echo 1; exit 1} }')
+  # don't like this sleep in a timed loop....
+  sleep 0.1
+done
+
 stop_reggie=$(date +%s%N)
 
 echo "Reggie took: " $(($stop_reggie-$start_reggie)) "ns"
-docker service rm compare_test
+docker service rm compare_test_1
 
 start_classic=$(date +%s%N)
 docker service create --mode global localhost:6000/large_image:unique_code compare_test
@@ -28,5 +36,5 @@ docker service ls -f name=reggie | awk 'NR==2 {split($4,a,"/"); if (a[1] != a[2]
 stop_classic=$(date +%s%N)
 
 
-echo "Classic took: " $(($stop_reggie-$start_reggie)) "ns"
+echo "Classic took: " $(($stop_classic-$start_classic)) "ns"
 
